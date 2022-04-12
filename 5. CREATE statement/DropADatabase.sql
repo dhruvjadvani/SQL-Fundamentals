7d05d8651c30871192ce64083778252adf7d4fed
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-12 23:25:00 +0530

    :)

--- 5. CREATE statement/DropADatabase.sql
+++ 5. CREATE statement/DropADatabase.sql
@@ -3,3 +3,6 @@ To delete a database, you can use the DROP DATABASE statement.
 */
 
 DROP DATABASE students;
+
+
+--Keep in mind that if you drop the database, you will lose all the tables stored in it
