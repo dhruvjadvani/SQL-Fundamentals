344a7129ea8477ef60269adb3325383871670847
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-12 23:32:50 +0530

    :)

---
+++ 5. CREATE statement/example.sql
@@ -0,0 +1,19 @@
+/*
+
+Create a table named staff with these columns:
+
+first_name (will store a string with a maximum length of 50 characters)
+last_name (will store a string with a maximum length of 50 characters)
+badge_id (will store a number)
+department (will store a string with a maximum length of 30 characters)
+position (will store a string with a maximum length of 15 characters)
+
+*/
+
+CREATE TABLE staff(
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    badge_id INTEGER,
+    department VARCHAR(30),
+    position VARCHAR(15)
+);
