e4012e8347e9bab64dc858c8e081765699d2b26c
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-12 23:16:45 +0530

    :)

--- 4. Arithmetic expressions/code.sql
+++ 4. Arithmetic expressions/code.sql
@@ -1,1 +1,9 @@
+SELECT -2;
+SELECT 20*15;
+SELECT 3/5;
+SELECT 18%4;
+SELECT 30+234;
+SELECT 3-5;
 
+/* SQL supports the common rules of operator precedence.
+In the list above operators are sorted by the decrease of priority. SQL also supports brackets to make an operator take priority over any other.*/
