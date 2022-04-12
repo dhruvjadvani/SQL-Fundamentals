1cdc91636c2ee6dc0e1f7241e0fbc7cdf695987c
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-12 22:49:51 +0530

    :)

--- 1. Data types/code.sql
+++ 1. Data types/code.sql
@@ -1,3 +1,6 @@
+/* Let's consider an example of an SQL query that defines a table "census" with 5 columns: "id" of type INTEGER,
+"name" of type VARCHAR(20), "birth_place_latitude" of type REAL, "year_income" of type DECIMAL(20,3), and "is_parent" of type BOOLEAN. */
+
 CREATE TABLE census (
     id INTEGER,
     name VARCHAR(20),
