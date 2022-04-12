ca5950741f0c134ab7bfae1847bbc11cf94ee4dd
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-13 00:19:04 +0530

    :)

---
+++ 7. Relational Data Model/code.sql
@@ -0,0 +1,5 @@
+/*
+The Relational Data Model (RDM) is an abstract mathematical model on the basis of which modern databases are designed. 
+Here's the list of several relational DBMS: Postgres, MySQL, Oracle, etc.
+*/
+
