737c9eb3f5e432cbaf3fd5f8cc7ee7e437c27837
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-12 23:35:18 +0530

    :)

---
+++ 5. CREATE statement/example1.sql
@@ -0,0 +1,8 @@
+--Create a table named persons with five columns: person_id (INT), name (VARCHAR(20)), surname (VARCHAR(20)), birth_date (DATE), and city (VARCHAR(25)).
+CREATE TABLE persons(
+    person_id INT, 
+    name VARCHAR(20), 
+    surname VARCHAR(20), 
+    birth_date DATE, 
+    city VARCHAR(25)
+);
