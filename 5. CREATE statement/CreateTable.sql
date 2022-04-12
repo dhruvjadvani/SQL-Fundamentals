3ab8920f3ddfa58549695773a2e23ad83e8e6ecd
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-12 23:22:42 +0530

    :)

---
+++ 5. CREATE statement/CreateTable.sql
@@ -0,0 +1,12 @@
+/*
+Let's keep working with our students database and create a table students_info that will contain four columns: student_id, name, surname and age.
+The column student_id will hold the unique student identifier of the INT type. The columns name and surname will have VARCHAR(30) data. 
+The age column will hold INT values.
+*/
+
+CREATE TABLE students_info ( 
+    student_id INT, 
+    name VARCHAR(30), 
+    surname VARCHAR(30), 
+    age INT
+);
