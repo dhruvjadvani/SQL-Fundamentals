5d5ea8a2fb766b85636b4a1cf8fbf3cd44cb104b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-14 23:47:35 +0530

    :)

--- 9. SELECT FROM Statement/code.sql
+++ 9. SELECT FROM Statement/code.sql
@@ -1,1 +1,36 @@
+SELECT * FROM table_name;
+
+SELECT
+    day, 
+    hour,
+    phenomena,
+    temperature as "temperature in Celsius",
+    feels_like as "feels like in Celsius",
+    wind_speed as "wind speed in m/s"
+FROM
+    weather
+;
+
+
+
+SELECT
+    col1 [AS alias1], ..., colN [AS aliasN]  
+FROM
+    table_name
+;
+
+
+
+
+SELECT
+    'London' as place,
+    day, 
+    hour,
+    phenomena,
+    temperature*9/5+32 as "temperature in Fahrenheit",
+    feels_like < temperature as "feels colder",
+    wind_speed as "wind speed in m/s"
+FROM
+    weather
+;
 
