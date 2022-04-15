39c5b39c2d128c4c444883987ea1eee5d8528f42
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-16 00:32:03 +0530

    :

---
+++ 11. Aggregate functions/code.sql
@@ -0,0 +1,15 @@
+SELECT AGG_FUNCTION(column_name)
+FROM table_name;
+
+/*
+Vendors of different SQL-compatible database management systems provide their users with slightly different sets of aggregate functions. 
+Here is a list of aggregate functions according to the ANSI SQL standard:
+
+MIN
+MAX
+AVG
+COUNT
+SUM
+*/
+
+
