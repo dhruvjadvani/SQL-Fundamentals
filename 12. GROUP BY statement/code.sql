c9474b4e7b7a4b815a0e34e35b5adff33a67039c
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-23 23:25:07 +0530

    :)

--- 12. GROUP BY statement/code.sql
+++ 12. GROUP BY statement/code.sql
@@ -44,6 +44,17 @@ GROUP BY
     datetime
 HAVING
     MAX(price) > 50;
+    
+    
+/*A fair question would be "why can't we utilize WHERE for the filtering"? The reason for that is the order of evaluation of the statements:
+
+FROM
+WHERE
+GROUP BY
+HAVING
+SELECT
+ORDER BY
+All the conditions that you put in HAVING have to relate to aggregation functions. However, besides that there are no other special restrictions. */
 
 
 SELECT column_name [, list_of_other_columns]
