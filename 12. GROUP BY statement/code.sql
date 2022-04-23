5a2d04dd33db585873b8fe6928f4fa849843e736
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-23 23:22:54 +0530

    :)

--- 12. GROUP BY statement/code.sql
+++ 12. GROUP BY statement/code.sql
@@ -31,7 +31,19 @@ GROUP BY
     stock_name,
     datetime;
     
-    
+ 
+ 
+SELECT
+    stock_name,
+    datetime,
+    MAX(price) AS maximum
+FROM
+    stock
+GROUP BY 
+    stock_name,
+    datetime
+HAVING
+    MAX(price) > 50;
 
 
 SELECT column_name [, list_of_other_columns]
