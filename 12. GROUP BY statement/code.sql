61cb4ca1df209aa4ed1850e1a1f7041871b9fa34
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-23 22:50:48 +0530

    :)

--- 12. GROUP BY statement/code.sql
+++ 12. GROUP BY statement/code.sql
@@ -6,6 +6,41 @@ FROM
     stocks
 GROUP BY
     stock_name;
+ 
+ 
+ 
+SELECT
+    stock_name,
+    MIN(datetime) AS moment,
+    MAX(price) AS maximum
+FROM
+    stocks
+GROUP BY
+    stock_name;
     
+
+
+
+SELECT
+    stock_name,
+    datetime,
+    MAX(price)
+FROM
+    stocks
+GROUP BY
+    stock_name,
+    datetime;
+    
+    
+
+
+SELECT column_name [, list_of_other_columns]
+     , aggregation [, list_of_aggregations]
+FROM table_name
+[WHERE list_of_conditions]
+GROUP BY column_name [, list_of_other_columns]
+[HAVING list_of_aggregate_conditions]
+[ORDER BY list_of_columns/aliases];
+
     
 
