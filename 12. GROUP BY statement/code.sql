63d5f27a550f1c43d48bb61bb03230781a606d1a
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-25 09:01:19 +0530

    :)

--- 12. GROUP BY statement/code.sql
+++ 12. GROUP BY statement/code.sql
@@ -117,3 +117,25 @@ SELECT distinct racer_id as racer , segment_id as segment , MIN(time) as "minimu
 FROM racing
 GROUP BY racer_id, segment_id
 order by min(time);
+
+
+
+/*
+In this task, you need to write a query that computes average budget and average box office for every year presented in table movies.
+Sort the result by year in ascending order.
+
+title	release_year	budget	box_office
+The Aviator	2004	110	121
+The Big Short	2015	28	133
+The Legend of Tarzan	2016	180	356
+Spotlight	2015	20	98
+Ex Machina	2015	10	36
+The Shape of Water	2017	19	195
+The Accountant	2016	44	155
+*/
+
+
+SELECT release_year, avg(budget) as avg_budget, avg(box_office) as avg_box_office
+from movies
+group by release_year
+order by release_year asc;
