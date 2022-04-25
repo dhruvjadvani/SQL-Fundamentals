6f3d2519eeeea007b19ee2b31a95579d8571f95d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-25 16:16:32 +0530

    :)

--- 13. Results Ordering/code.sql
+++ 13. Results Ordering/code.sql
@@ -1,1 +1,19 @@
+/*
+When you query data, SQL does not provide any default order of rows in the query evaluation result.
+To specify the order of the resulting rows, you should use the ORDER BY clause in the query.
+*/
+
+
+SELECT
+    hotel_id, 
+    hotel_name, 
+    price_per_night,
+    price_for_early_check_in,
+    rating, 
+    stars
+FROM 
+    hotels
+ORDER BY
+    price_per_night 
+;
 
