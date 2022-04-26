b2e3458aa3157b92ba151410b88391e93635df76
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-26 12:30:48 +0530

    :)

--- 14. Types of JOINS/code.sql
+++ 14. Types of JOINS/code.sql
@@ -1,1 +1,8 @@
+/*When you build a database it is more convenient to store information in several tables. Indeed, it avoids redundancy, makes tables easier to maintain, and establishes the right connections between them.
+Working on real projects, you will often need to use more than one table to get the information you want.
+
+This is where JOINs come in. In theory, SQL join is a SQL (Structured Query Language) instruction to combine records from two tables based on a search condition.
+Records are joined when the search condition evaluates to true. The resulting row consists of all the columns from both joined tables.
+*/
+
 
