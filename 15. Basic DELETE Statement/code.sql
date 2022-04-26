2504bcd6a0b70fc96c26685edcd26c3a4bd6adc5
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-26 15:22:30 +0530

    :)

--- 15. Basic DELETE Statement/code.sql
+++ 15. Basic DELETE Statement/code.sql
@@ -1,1 +1,7 @@
+--To delete all the data from the table but not the table itself, use a query with a DELETE FROM statement.
+DELETE FROM books 
+--As a result of the query execution we still have the table books, but it is empty now.
+Delete selected rows
+You can also use DELETE FROM statement to delete only selected rows from a table.
+
 
