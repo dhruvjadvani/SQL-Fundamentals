455a1f86218c58474860a0313e0997611b34eeb6
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-26 17:17:05 +0530

    :)

--- 15. Basic DELETE Statement/code.sql
+++ 15. Basic DELETE Statement/code.sql
@@ -11,3 +11,11 @@ DELETE FROM table_name
 DELETE FROM table_name 
 WHERE logical_expression
 
+--Delete all records about Jane Smith from table guests.
+
+DELETE FROM guests
+WHERE name = 'Jane Smith';
+
+--Delete all the movies which have been released after 1980 (including this year) and have rating less than 8.5.
+DELETE FROM movies
+WHERE year >= 1980 AND rating < 8.5;
