f03e5eb97eaa7b50b296cbd49e04fd4483a041f1
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-26 12:28:02 +0530

    :)

--- 13. Results Ordering/code.sql
+++ 13. Results Ordering/code.sql
@@ -153,3 +153,26 @@ ORDER BY
     stars ASC,
     rating DESC,
     price_per_night * 5 + price_for_early_check_in ASC;
+
+
+/*
+A table "Titanic_passengers" that stores info about the passengers of the Titanic has the following columns:
+
+integer passenger_id
+boolean survived
+integer passenger_class
+varchar(20) first_name
+varchar(20) last_name
+boolean is_male
+integer age (in 1912)
+integer sibsp (number of siblings/spouses aboard)
+integer parch (number of parents/children aboard)
+Write a query that selects all the info about each passenger. Sort the results by the number of relatives aboard (large families should go first).
+*/
+
+SELECT * FROM Titanic_passengers
+ORDER BY 
+    sibsp DESC,
+    parch DESC;
+
+
