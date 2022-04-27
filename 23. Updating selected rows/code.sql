f9a98f4b7e7e642f71e6e4e6642ec9efe24932a1
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 00:34:17 +0530

    :)

--- 23. Updating selected rows/code.sql
+++ 23. Updating selected rows/code.sql
@@ -1,1 +1,15 @@
+--general template of an update statement
+UPDATE table_name 
+SET 
+    col1 = expr1, 
+    col2 = expr2, 
+    …, 
+    colN = expr 
+WHERE 
+    logical_expression;
+    
+    
+    
+
+
 
