69c53c1bc07ebcf493c366034dd3461c9c50c45b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 12:41:38 +0530

    Update code.sql

--- 23. Updating selected rows/code.sql
+++ 23. Updating selected rows/code.sql
@@ -51,6 +51,36 @@ WHERE
     street_name = 'Water Sports Arena';
     
 
+--Games
+--Because of a sudden crisis in the video game industry, two titles are going to change their publishers.
+
+/*
+"Bridges" and "Ship Simulator 2" will from now on be distributed by "Macrofiber".
+Unfortunately, gamers didn't really like that, so users' ratings of both titles dropped by 2.5. Update the games table accordingly.
+*/
+
+UPDATE games
+SET 
+    publisher = 'Macrofiber',
+    users_rating = users_rating - 2.5
+WHERE
+    title IN ('Bridges', 'Ship Simulator 2');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
