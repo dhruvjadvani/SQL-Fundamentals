e6d594b908eda7ecae968e695e41918016b742d2
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 18:53:42 +0530

    :)

--- 27. Consistency constraints/code.sql
+++ 27. Consistency constraints/code.sql
@@ -196,6 +196,23 @@ ADD CONSTRAINT uq_names UNIQUE (name);
 ALTER TABLE furniture
 ALTER type SET DEFAULT 'chair';
 
+--No more mistakes
+/*
+Correct the query that should add a named CHECK constraint that will check the age and experience columns in the table candidates. 
+The name of the constraint should be chk_candidates.
+Age should be greater than or equal to 18, and experience should be greater than 1.
+
+ALTER TABLE
+ADD chk_candidates CHECK (age>=18 or experience>1);
+*/
+
+
+ALTER TABLE candidates
+ADD CONSTRAINT chk_candidates CHECK (age >= 18 AND experience > 1);
+
+
+
+
 
 
 
