3ededa05c05de42b605bec3fec0e24c3432777d3
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 12:52:28 +0530

    :)

--- 24. Intro to MYSQL/code.sql
+++ 24. Intro to MYSQL/code.sql
@@ -1,1 +1,131 @@
+--MySQL is one of the most popular Relational Database Management Systems in the world.
+
+/*
+MySQL introduces a relational database management system (RDBMS). It allows users to interact with databases (view, search, add and manage data).
+
+MySQL is developed by Oracle — the largest manufacturer of software for organizations. The official website of MySQL is https://www.mysql.com.
+There you can get detailed information about the product, its developers, and installation instructions.
+*/
+
+--SQL and MySQL
+/*
+In a nutshell, SQL is a language for querying databases and MySQL is a database management service.
+SQL is the language used to manage data in a database. MySQL is a database management system that allows storing data in an organized database. 
+Furthermore, exactly the database management system (MySQL in our case) provides your interaction with data using an SQL query language.
+
+For example, there are also many other RDBMS except MySQL: Microsoft SQL Server, PostgreSQL, Oracle Database, and others. All of them use SQL as query language.
+*/
+
+
+
+/*
+Advantages of MySQL
+MySQL has several advantages that make this RDBMS in high demand. Let's look closer at the most important ones of them.
+
+Security. Data security in MySQL is an amazing benefit. Especially for applications where some confidential data or money transfers are processed.
+For example, in eCommerce, banking, and government websites.
+
+
+Scalability. MySQL provides great scalability that allows building high-load systems without taking many places on servers.
+This solution allows a complete customization database with unique project requirements.
+
+
+Performance. Thanks to high-performance MySQL can fast process lots of queries. So you can build high-load systems without having to worry about performance.
+
+
+Workflow control. You can manage all the features based on the needs of your project. MySQL is also cross-platform, so it can easily work at Windows, Linux, and macOS.
+*/
+
+
+/*
+Disadvantages of MySQL
+Like any other technology, MySQL has some disadvantages as well. Let's talk about some of them.
+
+Big amounts of data. MySQL is not very efficient in handling very large databases.
+
+
+Hard to debug. MySQL doesn't have as good a developing and debugging tool as compared to the paid database management systems.
+
+
+Transactions. MySQL doesn't handle transactions very efficiently and is prone to data corruption.
+
+
+Despite this MySQL is still a very good solution for different projects. Therefore MySQL is used by Uber, Netflix, Amazon, Twitter, and other big IT companies.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
