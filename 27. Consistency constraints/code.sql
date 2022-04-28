02a73d0fdf642fa14b919a54c7c0349cc1f86ee4
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 18:42:58 +0530

    :)

--- 27. Consistency constraints/code.sql
+++ 27. Consistency constraints/code.sql
@@ -55,6 +55,145 @@ CREATE TABLE employees (
 
 
 
+/*
+UNIQUE constraint
+The UNIQUE constraint will prohibit adding duplicate values to the column.
+We know that the employee identifier should be unique, so we can add this constraint to the column personal_id.
+
+If you want to add a UNIQUE constraint to the column when creating the table, you can specify it after column type declaration:
+*/
+
+CREATE TABLE employees (
+    personal_id INT UNIQUE,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    age INT 
+);
+
+
+--We can add UNIQUE to an existing table with a simple query using ALTER TABLE ADD UNIQUE statement:
+ALTER TABLE employees
+ADD UNIQUE (personal_id);
+
+
+--After the query execution, column personal_id becomes unique.
+--If we already have duplicate values in this column, we will get an error, so check your table before adding this constraint.
+
+
+--Sometimes we have to make more than one column unique. In this case, we can define a named constraint at the end of the CREATE TABLE statement:
+
+CREATE TABLE employees (
+    personal_id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    age INT, 
+    CONSTRAINT uq_id_last_name UNIQUE (personal_id, last_name) 
+);
+
+
+--To drop a named constraint, you can also use ALTER TABLE DROP INDEX statement:
+ALTER TABLE employees
+DROP INDEX uq_id_last_name; 
+
+
+
+/*
+CHECK constraint
+The CHECK constraint allows us to add a logical expression. For example, we can say that all our employees should be older than sixteen.
+We can add the CHECK constraint in CREATE TABLE:
+*/
+
+
+CREATE TABLE employees (
+    personal_id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    age INT CHECK (age > 16)
+);
+
+
+--If you want to add this constraint to an existing table, you can use the ALTER TABLE ADD CHECK statement. The syntax will be the same as with the UNIQUE constraint.
+--To drop an unnamed CHECK, use ALTER TABLE DROP CHECK:
+
+ALTER TABLE employees
+DROP CHECK age; 
+
+
+/*
+The CHECK constraint can be named and assigned to multiple columns.
+You can define a named CHECK constraint when creating a table like in the UNIQUE example above.
+You can also use the ALTER TABLE ADD CONSTRAINT statement and add a named CHECK constraint for one or multiple columns to an existing table with the following query:
+*/
+
+
+ALTER TABLE employees 
+ADD CONSTRAINT chk_employee CHECK (age > 16 AND personal_id > 0); 
+
+/*
+The ALTER TABLE ADD CONSTRAINT statement can be used to add both the CHECK constraint and a named UNIQUE constraint.
+
+To delete a named CHECK constraint, you can use the ALTER TABLE DROP CHECK statement.
+*/
+
+/*
+DEFAULT constraint
+The DEFAULT constraint defines the initial value in a column: the value that will appear if you don't insert anything.
+Let's define some default values in our table:
+*/
+
+
+CREATE TABLE employees (
+    personal_id INT,
+    first_name VARCHAR(30) DEFAULT 'John',
+    last_name VARCHAR(30) DEFAULT 'Doe',
+    age INT DEFAULT 17
+);
+
+
+
+--Now even if you add a new row with only personal_id, the columns first_name, last_name, and age will be defined as 'John', 'Doe', and 17 respectively.
+
+--To add the DEFAULT constraint to an existing table, use the ALTER TABLE ALTER SET DEFAULT statement:
+
+ALTER TABLE employees
+ALTER first_name SET DEFAULT 'John';
+
+
+--To delete an existing DEFAULT constraint, use the ALTER TABLE ALTER DROP DEFAULT statement:
+
+ALTER TABLE employees 
+ALTER first_name DROP DEFAULT;
+
+
+--Combining constraints
+/*
+Obviously, a column can have more than one constraint. For example, 
+it is useful to combine NOT NULL and DEFAULT constraints to avoid errors when adding some new information.
+
+Let's re-create our table employees using all these useful constraints at the same time:
+*/
+
+
+CREATE TABLE employees (
+    personal_id INT NOT NULL UNIQUE,
+    first_name VARCHAR(30) NOT NULL DEFAULT 'John',
+    last_name VARCHAR(30) NOT NULL DEFAULT 'Doe',
+    age INT DEFAULT 17, 
+    CHECK (age > 16) 
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
