526c24a5d957306d5a16d36db4ea5fd95e8b8af8
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 21:34:59 +0530

    :)

--- 28. Foreign constraints/code.sql
+++ 28. Foreign constraints/code.sql
@@ -12,4 +12,58 @@ In this topic, you will learn how to use the foreign key constraint to connect t
 
 
 --FOREIGN KEY constraint
+--Note that the FOREIGN KEY constraints can only reference tables within the same database
+
+
+/*
+To mark a field or a group of fields as a foreign key, we can use the FOREIGN KEY constraint and create the table employees. In the query,
+keyword REFERENCES specifies the table and the primary key column or columns (in parentheses) with unique values that the foreign key points to. 
+The structure and data type of the primary key and the foreign key must be the same. The table that the foreign key points to is called referenced or parent table. 
+Here is what the query will look like:
+*/
+
+
+CREATE TABLE employees (
+    employee_id int PRIMARY KEY,
+    name VARCHAR(60) NOT NULL, 
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id)
+    REFERENCES departments(department_id)
+); 
+--After the query execution, the table employees becomes a child table, that is, a table containing the foreign key.
+
+
+
+/*
+With the query above, we created a named foreign key. 
+It is good practice to use only named foreign keys because in some SQL dialects (including MySQL),
+unnamed foreign keys may cause trouble in the future. However, you can still create a table with an unnamed foreign key:
+*/
+
+CREATE TABLE employees (
+    employee_id int PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
