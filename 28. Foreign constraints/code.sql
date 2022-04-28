564a57d565337fd254e6e204cbd05db01980152b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 21:56:51 +0530

    :)

--- 28. Foreign constraints/code.sql
+++ 28. Foreign constraints/code.sql
@@ -134,8 +134,20 @@ ADD CONSTRAINT fk_customers FOREIGN KEY (customer_id)
 REFERENCES customers(customer_id);
 
 
+--Pets
+/*
+Create a table animals with columns name VARCHAR(20), 
+animal_kind VARCHAR(20) and owner_id INT. The owner_id column should be an unnamed foreign key pointed to the column id in the table persons.
+*/
 
 
+CREATE TABLE animals(
+    name VARCHAR(20),
+    animal_kind VARCHAR(20),
+    owner_id INT,
+    CONSTRAINT FOREIGN KEY (owner_id)
+    REFERENCES persons(id)   
+);
 
 
 
