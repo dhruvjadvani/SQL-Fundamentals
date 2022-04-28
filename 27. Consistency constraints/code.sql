69db8699dc8a7f82bd576001a36996d954c7343e
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 18:56:59 +0530

    :)

--- 27. Consistency constraints/code.sql
+++ 27. Consistency constraints/code.sql
@@ -212,6 +212,35 @@ ADD CONSTRAINT chk_candidates CHECK (age >= 18 AND experience > 1);
 
 
 
+--The girl has no name
+/*
+Create a table users with columns name VARCHAR(40), age INT, and city VARCHAR(20).
+
+The column name should be NOT NULL.
+*/
+
+CREATE TABLE users (
+    name VARCHAR(40) NOT NULL,
+    age INT,
+    city VARCHAR(20)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
