687e6a4a7c3ffe61555aaa46a5d6b77d33338a4d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 21:53:07 +0530

    :)

--- 28. Foreign constraints/code.sql
+++ 28. Foreign constraints/code.sql
@@ -126,7 +126,12 @@ ADD CONSTRAINT fk_id FOREIGN KEY(id_number)
 REFERENCES documents(id_number)
 ON DELETE CASCADE;
 
+--Link the tables
+--To the column customer_id in the table orders, add a named foreign key constraint fk_customers referring to the primary key column customer_id in the table customers.
 
+ALTER TABLE orders
+ADD CONSTRAINT fk_customers FOREIGN KEY (customer_id)
+REFERENCES customers(customer_id);
 
 
 
