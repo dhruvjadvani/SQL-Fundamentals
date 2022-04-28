ff1ef4c13854229830409f3134de5e69adbb9870
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-28 22:08:36 +0530

    :)

--- 29. Date and time datatypes/code.sql
+++ 29. Date and time datatypes/code.sql
@@ -1,1 +1,67 @@
+/*
+SQL works with data of different types organized into tables. You already know some basic data types that allow you to store numbers, strings, or logical data. 
+There's another thing we face every day. Think of what you do first when you wake up – yeah, you check what's the time! We face date and time data regularly
+SQL provides a wide variety of date and time types.
+*/
+
+/*
+Date & Time types
+In different SQL dialects, date and time types may differ, but here we will talk about the most common ones: DATE, TIME, DATETIME, TIMESTAMP, and INTERVAL.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
